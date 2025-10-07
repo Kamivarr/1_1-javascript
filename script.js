15956a175b0db7010726c508d391b9c7d62ad331
Kamil Śliwa <137560154+Kamivarr@users.noreply.github.com>
2025-10-07 09:10:31 +0000

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-(function () {
-  //TODO
+(function() {
+  // Pobieramy przycisk i element, w którym wyświetlimy liczby
+  var ex1_button = document.getElementById('ex1_button');
+  var ex1_content = document.getElementById('ex1_content');
+
+  // Przypisujemy funkcję do zdarzenia onclick
+  ex1_button.onclick = function() {
+    var tabela = [];
+
+    // Generujemy liczby od 0 do 9
+    for (var i = 0; i <= 9; i++) {
+      tabela.push(i);
+    }
+
+    // Wyświetlamy liczby jako ciąg znaków oddzielony przecinkami
+    ex1_content.innerHTML = tabela.toString();
+  };
 })();
