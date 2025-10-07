46a2c171e991423d280d655f702f047a14c3b5f8
Kamil Śliwa <137560154+Kamivarr@users.noreply.github.com>
2025-10-07 09:17:43 +0000

    Com1_1.2

--- script.js
+++ script.js
@@ -14,5 +14,24 @@
 
     // Wyświetlamy liczby jako ciąg znaków oddzielony przecinkami
     ex1_content.innerHTML = tabela.toString();
-  };
+
+
+  }
+  const ex2_text = document.getElementById('ex2_text');
+  const ex2_content = document.getElementById('ex2_content');
+
+  ex2_text.addEventListener('input', function() {
+    const value = ex2_text.value;
+
+    // Kolejność walidacji:
+    if (/[a-zA-Z]/.test(value)) {
+      ex2_content.textContent = "Numer nie może zawierać liter";
+    } else if (/[^0-9]/.test(value)) {
+      ex2_content.textContent = "Numer nie może zawierać znaków specjalnych";
+    } else if (value.length !== 9) {
+      ex2_content.textContent = "Długość numeru musi być równa 9";
+    } else {
+      ex2_content.textContent = "Numer telefonu jest poprawny";
+    }
+  });
 })();
